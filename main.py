10c49176877da0c2cd3290ab848fe79c02594de3
JonasKennedy <93797170+jkgprep@users.noreply.github.com>
2021-11-23 18:40:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#Jonas Kennedy, 11/23/2021, program that counts down from given user number
+import time
+start = int(input("What number would you like to countdown from \n"))
+
+
+seconds = time.time()
+print("Seconds since epoch =", seconds)	
